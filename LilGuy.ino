dcc4f8c84ae3fcf8f57fe941e6d4a3c178a93082
JonU238 <Jamestfishes@gmail.com>
2025-01-31 14:49:58 -0500

    Renamed vars and made run maybe a little faster?

--- LilGuy.ino
+++ LilGuy.ino
@@ -1,37 +1,36 @@
-#include "LSM6DS3.h"
+#include <LSM6DS3.h>
 #include "Wire.h"
 
 LSM6DS3 lilIMU(I2C_MODE, 0x6A);
-float totalAcc = 0;
-int threashold = 0;
-int sampleRate = 10; //ms delay per cycle (should be low AF)
-int boostTime = 250;//Length of time to feel g's before going "AAHHHAHA"
-int boostThreshold = 2;//G's
-int onTime = 1;//Min
+float currentAccMagnitude = 0;//In G's
+int counter = 0;
+int sampleRate = 1000;//HZ
+int boostThreshold = 5;//G's
+int boostTimeMS = 250;//Length of time to feel g's before going "AAHHHAHA"
+
+int onTime = 1;//Time Camera is on in minutes after detecting boost
 
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  //Call .begin() to configure the IMUs
-lilIMU.begin();
+    Serial.begin(115200); 
+    lilIMU.begin();
+    sampleRate = (1/sampleRate)*1000;// converts HZ to MS to wait at end of loop, hoping that its fast enough to not mater
 }
 
 void loop(){
-    totalAcc = sqrt(pow(lilIMU.readFloatAccelX(), 2) + pow(lilIMU.readFloatAccelY(), 2) + pow(lilIMU.readFloatAccelZ(), 2));
-    Serial.println(totalAcc);
-    delay(10);
-    if (totalAcc >= boostThreshold) {
-        threashold++;
+    currentAccMagnitude = pow(lilIMU.readFloatAccelX(), 2) + pow(lilIMU.readFloatAccelY(), 2) + pow(lilIMU.readFloatAccelZ(), 2);
+    Serial.println(currentAccMagnitude);
+    if (currentAccMagnitude > pow(boostThreshold, 2)) {
+        counter++;
     }
-    else if (threashold > 0){
-        threashold--;
+    else if (counter > 0){
+         counter--;
     }
-    if (threashold >= boostTime/sampleRate){
-        threashold = 0;
+    if (counter > boostTimeMS/sampleRate){
+        counter = 0;
         Serial.println("BOOST");
-        delay(onTime*1000*60);
+        delay(onTime*60000);//Min to MS
     }
-    
+    delay(sampleRate);
 }
